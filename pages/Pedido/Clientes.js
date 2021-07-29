f68e9b85ac03e7aae5e9cc32aec0db04776a4b7d
Orlin Samuel Perez <orlinsampa28@gmail.com>
2021-07-28 22:35:36 -0400

    Prueba 2

--- pages/Pedido/Clientes.js
+++ pages/Pedido/Clientes.js
@@ -1,5 +1,5 @@
-import FormCliente from '../Components/Forms/FormCliente';
-import ContainerForm from "../Components/Forms/ContainerForm";
+import FormCliente from '../../Components/Forms/FormCliente';
+import ContainerForm from "../../Components/Forms/ContainerForm";
 
 export default function PedidoProveedor(){
     
